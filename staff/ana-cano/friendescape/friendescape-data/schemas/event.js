12d0e5918944e72d6cfc2c9b5cc937809465ade3
Hanytahanyta <anacano1985@gmail.com>
2020-03-09 19:39:14 +0100

    init modules

---
+++ staff/ana-cano/friendescape/friendescape-data/schemas/event.js
@@ -0,0 +1,16 @@
+const { Schema, Types: { ObjectId } } = require('mongoose')
+
+module.exports = new Schema({
+
+    title: { type: String, required: true, ref: 'roomEscape'},
+    location: { type: String, required: true, ref: 'roomEscape'},
+    date: { type: Date, required: true },
+    time: { type: String, required: true},
+    minplayers: { type: Number, required: true, ref: 'roomEscape'},
+    img: { type: String, required: true, ref: 'roomEscape'},
+    maxplayers: { type: Number, required: true, ref: 'roomEscape' },
+    publisher: { type: ObjectId, required: true, ref: 'User' },
+    created: { type: Date, required: true, default: Date.now },
+    subscribed: [{ type: ObjectId, ref: 'User' }],
+    state: [{ type: String, required: true}]
+})
---
+++ staff/ana-cano/friendescape/friendescape.doc/README.md
@@ -0,0 +1,8 @@
+## FRIENDESCAPE
+
+![](https://media.giphy.com/media/12pWOEeKbbfdio/giphy.gif)
+<p>FriendEscape is a application where you will have the chance to find information about escapes rooms in Barcelona. You will find information about price, groups, duration, different themes,... You also will have the chance to create new groups in order to be able to join one escape room, if you have done a reservation and your group can't go, here you will find new friends ..., or on the other hand if you find a group that is already created you can join them. 
+</p>
+
+
+
