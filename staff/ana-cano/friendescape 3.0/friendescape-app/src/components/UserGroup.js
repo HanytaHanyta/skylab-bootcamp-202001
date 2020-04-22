f4625108e91e20b6052395d89baa1d196dd0fd27
Hanytahanyta <programadoraenapuros@gmail.com>
2020-04-22 13:30:43 +0200

    docu

--- staff/ana-cano/friendescape 3.0/friendescape-app/src/components/UserGroup.js
+++ staff/ana-cano/friendescape 3.0/friendescape-app/src/components/UserGroup.js
@@ -1,15 +1,11 @@
 import Moment from 'react-moment'
-import React, { useState } from 'react'
+import React from 'react'
 import '../sass/components/group.sass'
-import Feedback from './Feedback'
 
-import {isOwner, isJoined} from '../logic'
 
 
 export default function ({ item, handleJoinGroup, handleLeaveGroup, handleDeleteGroup, error, user }) {
-    const [warning, setWarning] = useState(false)
-    const [warningDelete, setWarningDelete] = useState(false)
-    const [warningLeave, setWarningLeave] = useState(false)
+    
     const { id, date, time,  subevents, state, escapeRoom} = item
     
     let style = ""
@@ -52,74 +48,7 @@ export default function ({ item, handleJoinGroup, handleLeaveGroup, handleDelete
             <h5>Max-Players: <span>{escapeRoom.maxplayers}</span></h5>
        </li>
             
-            {/* {!user.subbedTo[0]._id === id && ( */}
-            {!isOwner(user, id) && !isJoined(item) && subevents.length < escapeRoom.maxplayers  && <a href="" className="group__btn" onClick={e => {
-                e.preventDefault()
-                setWarning(!warning)
-            }}><i class="fas fa-users"></i>Join team</a>}
-            {/* )} */}
-            {isOwner(user, id) && <a href="" className="group__btn" onClick={e => {
-                e.preventDefault()
-                setWarningDelete(!warning)
-            }}><i class="fas fa-trash-alt"></i>Delete team</a>}
-            {!isOwner(user, id) && isJoined(item) && <a href="" className="group__btn" onClick={e => {
-                e.preventDefault()
-                setWarningLeave(!warning)
-            }}><i class="fas fa-trash-alt"></i>Leave team</a>}
-            {!isOwner(user, id) && !isJoined(item) && subevents.length >= escapeRoom.maxplayers  && <a href="" className="group__btn" onClick={e => {
-                e.preventDefault()
-            }}><i class="fas fa-users"></i>This team is complete</a>}
-
-
-            
-            {warning && (<>
-            <p>Read carefully these instructions. If you press Join Group you will be a new member of the group and you will adquire one trusty point. In the other hand if you are a show off in the escape room you will receive a fault. If you commit 3 faults you will be banned.</p>
-            <button onClick={ event => {
-                const id = item.id
-                event.preventDefault();
-                setWarning(false)
-                handleJoinGroup(id)
-                }}>Understood</button>
-            <button onClick={event=> {
-                event.preventDefault();
-                setWarning(false)
-            }}>Cancel</button>
-            </>)}
-
-            {warningDelete && (<>
-            <p>Read carefully these instructions. If you press Delete Group you will receive a fault. If you commit 3 faults you will be banned. </p>
-            <button onClick={ event => {
-                const id = item.id
-                event.preventDefault();
-                setWarningDelete(false)
-                handleDeleteGroup(id)
-                }}>Understood</button>
-            <button onClick={event=> {
-                event.preventDefault();
-                setWarningDelete(false)
-            }}>Cancel</button>
-            </>)}
-
-            {warningLeave && (<>
-            <p>Read carefully these instructions. If you press Leave Team you will receive a fault. If you commit 3 faults you will be banned. </p>
-            <button onClick={ event => {
-                const id = item.id
-                event.preventDefault();
-                setWarningLeave(false)
-                handleLeaveGroup(id)
-                }}>Understood</button>
-            <button onClick={event=> {
-                event.preventDefault();
-                setWarningLeave(false)
-            }}>Cancel</button>
-            </>)}
-            
-            
-            {error && <Feedback message={error} level="warning"/>}
-            {/* <button onClick={() => {
-        alert.show('Read carefully these instructions. If you press Join Group you will be a new member of the group. If you are a show off you will receive a fault. If you commit 3 faults you will be banned.')
-      }} >Join Group
-    </button> */}
+         
 
 
         </li>
--- staff/ana-cano/friendescape 3.0/friendescape.doc/README.md
+++ staff/ana-cano/friendescape 3.0/friendescape.doc/README.md
@@ -2,17 +2,21 @@
 ![GIF](https://media.giphy.com/media/cEYFeDOOQ0cHqIgIEOA/source.gif)
 
 
-<p>FriendEscape is a application where you will have the chance to find information about escapes rooms in Barcelona. You will find information about price, groups, duration, different themes,... You also will have the chance to create new groups in order to be able to join one escape room, if you have done a reservation and your group can't go, here you will find new friends ..., or on the other hand if you find a group that is already created you can join them. 
+<p>FriendEscape is a application where you will have the chance to find information about escapes rooms in Barcelona. You will find information about price, groups, duration, different themes,... You also will have the chance to create new groups in order to be able to join one escape room, join one of the existing groups, leave an existing group or delete one.
+
 </p>
 
 ### FUNCTIONAL DESCRIPTION
 The user should be registered on the app to acces to all the options of the app. The available functionalities are: 
 * Check escape rooms
 * Check locations of the escape rooms
+* Search by difficulty
+* Search by theme
 * Acces to the reservation page of the oficial site
 * Check available groups
-* Create a new group
-* Join one of the groups
+* Create a team
+* Join one team
+* Leave a team
 * Check your profile
 * Check your teams
 
